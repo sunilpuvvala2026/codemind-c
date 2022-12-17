0861a36c1ea56f1d765ed29d7003b5232d5c8cc8
sunilpuvvala2026 <119731842+sunilpuvvala2026@users.noreply.github.com>
2022-12-17 15:44:05 +0530

    #include<stdio.h> int main() {     int x,a,b;     scanf("%d%d%d",&x,&a,&b);     a=a*1;     b=2*b;      if((a+b)>=x)     {         printf("Qualify");     }     else   {     printf("Not Qualify");         } }

---
+++ Contest:.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,a,b;
+    scanf("%d%d%d",&x,&a,&b);
+    a=a*1;
+    b=2*b;
+     if((a+b)>=x)
+    {
+        printf("Qualify");
+    }
+    else
+  {
+    printf("Not Qualify");       
+        }
+}
